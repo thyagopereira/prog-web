e06830863e894c5de585e047c70c1b4fc73def32
thyagopereira <thyago.pereira.silva@ccc.ufcg.edu.br>
2024-07-14 12:48:48 -0300

    Adicionando rotas e html

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
 const port = 3000; 
+const path = require('path');
 
 app.get('/', (req, res) => {
     res.send("Hello World!");
 });
+
+app.use('/index', express.static(path.join(__dirname, 'public')));
 
 app.listen(port, ()=> {
     console.log(`Server running at http://localhost:${port}/`);
